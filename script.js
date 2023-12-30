238c0493f814dbf4fcec926f846b3576bed2e290
HichamBENKADA <hicham20014@gmail.com>
2023-12-29 20:28:30 -0500

    index html is added to complete part3

--- script.js
+++ script.js
@@ -12,22 +12,48 @@
 //     console.log(`max: ${counter}`,err.stack);// I got 12555 range
 // }
 
-//part2: Trampolining eleminate stack overflow
+// //part2: Trampolining eleminate stack overflow
 
-//recursive to flatten an array of nasted arrays
+// //recursive to flatten an array of nasted arrays
 
-function flatten(arr){
-    if(arr.length === arr.flat().length) return arr;
-    arr = arr.flat();
-    return () => flatten(arr);
-}
+// function flatten(arr){
+//     if(arr.length === arr.flat().length) return arr;
+//     arr = arr.flat();
+//     return () => flatten(arr);
+// }
 
-function trampoline(fn,...arr){
-    let result = fn(...arr);
-    while (typeof result === "function"){
-        result = result();
+// function trampoline(fn,...arr){
+//     let result = fn(...arr);
+//     while (typeof result === "function"){
+//         result = result();
+//     }
+//     return result;
+// }
+
+// console.log(trampoline(flatten([21,3,[21,9,[2,4,7,9,[2,9,0]],[2,3]],21,3,[21,9,[2,4,7,9,[2,9,0]],[2,3]],21,3,[21,9,[2,4,7,9,[2,9,0]],[2,3,21,3,[21,9,[2,4,7,9,[2,9,0]],[2,3]]]]]))); 
+
+//part3: Deferred Execution
+
+const hOne = document.createElement("h1");
+function isPrime(n, i = 2){
+    if (n == i){
+        return true;
     }
-    return result;
+    else if(n % i ==0) {
+        return false;
+    }
+    return isPrime(n, i++)
 }
-
-console.log(trampoline(flatten([21,3,[21,9,[2,4,7,9,[2,9,0]],[2,3]],21,3,[21,9,[2,4,7,9,[2,9,0]],[2,3]],21,3,[21,9,[2,4,7,9,[2,9,0]],[2,3,21,3,[21,9,[2,4,7,9,[2,9,0]],[2,3]]]]])));
+document.body.appendChild(hOne)
+function addPrimeList(n){
+    hOne.textContent = `list of primes between 1 and ${n}:`
+    while(n>1){
+        if(isPrime(n)){ 
+        hOne.textContent +=`${n} ,`
+        }
+        n--;
+    }
+    
+    
+    setTimeout(()=>{alert('Calculation is finished')},0)
+}
